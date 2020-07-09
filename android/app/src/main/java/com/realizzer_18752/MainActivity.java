55385812ffd69131e8e832ecdff904515df03470
Crowdbotics <team@crowdbotics.com>
2020-07-09 15:08:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/realizzer_18752/MainActivity.java
@@ -0,0 +1,15 @@
+package com.realizzer_18752;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "realizzer_18752";
+    }
+}
